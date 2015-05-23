7a18e474333a2280b800f39c36330c80b1b93109
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-05-23 16:05:10 +0200

    implemented emit, context and transition dicitionaries

--- 2/markov.py
+++ 2/markov.py
@@ -6,8 +6,16 @@ class Model:
 
     def __train(self, word_tag_sentences):
         self.context = defaultdict(int)
+        self.transition = defaultdict(int)
+        self.emit = defaultdict(int)
         for sentence in word_tag_sentences:
+            previous = '<s>'
+            self.context[previous] += 1
             for word, tag in sentence:
+                self.transition[(previous, tag)] += 1
                 self.context[word] += 1
+                self.emit[(tag, word)] += 1
+                previous = tag
+            self.transition[(previous, '</s>')] += 1
 
 
--- 2/test_model.py
+++ 2/test_model.py
@@ -9,13 +9,33 @@ class TestModel:
     def model(self, word_tag_sentences):
         return markov.Model(word_tag_sentences)
 
-    def test_word_sentence(self):
+    def example_model(self):
         word_tag_sentences = [
                 [("I", "NN"), ("am", "VB"), ("a", "DT"), ("sentence", "NN")],
                 [("I", "NN"), ("love", "VB"), ("the", "DT"), ("sun", "NN")]
         ]
-        model = self.model(word_tag_sentences)
+        return self.model(word_tag_sentences)
+
+    def test_context(self):
+        model = self.example_model()
         assert model.context['I'] == 2
         assert model.context['love'] == 1
         assert model.context['sun'] == 1
+        assert model.context['<s>'] == 2
+        assert model.context['</s>'] == 0 # okay, we transition the end of a sentence but don't count it, is that correct?
+
+    def test_transition(self):
+        model = self.example_model()
+        assert model.transition[('<s>', 'NN')] == 2
+        assert model.transition[('NN', '</s>')] == 2
+        assert model.transition[('NN', 'VB')] == 2
+        assert model.transition[('VB', 'NN')] == 0
+
+    def test_emit(self):
+        model = self.example_model()
+        assert model.emit[('VB', 'am')] == 1
+        assert model.emit[('NN', 'sentence')] == 1
+        assert model.emit[('DT', 'the')] == 1
+        assert model.emit[('NN', 'the')] == 0
+
 
