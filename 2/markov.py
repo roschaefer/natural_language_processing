88862f345f43828e32f15770263013503c960b3c
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-06-01 00:32:06 +0200

    more reasonable text example, implemented prob_e and prob_t

--- 2/markov.py
+++ 2/markov.py
@@ -1,10 +1,13 @@
 from collections import defaultdict
+import math
+
 class Model:
     def __init__(self, word_tag_sentences):
         self.word_tag_sentences = word_tag_sentences
         self.__train(word_tag_sentences)
 
     def __train(self, word_tag_sentences):
+        self.tag_unigrams = defaultdict(int)
         self.context = defaultdict(int)
         self.transition = defaultdict(int)
         self.emit = defaultdict(int)
@@ -13,9 +16,32 @@ class Model:
             previous = '<s>'
             self.context[previous] += 1
             for word, tag in sentence:
+                self.tag_unigrams[tag] += 1
                 self.transition[(previous, tag)] += 1
                 self.context[word] += 1
                 self.emit[(tag, word)] += 1
                 self.possible_tags.add(tag)
                 previous = tag
             self.transition[(previous, '</s>')] += 1
+
+
+    def test(self, sentence):
+        self.current_sentence = sentence
+        return None
+
+    def prob_t(self, tag, previous_tag):
+        # TODO: smoothing
+        return float(self.transition[(previous_tag, tag)]) / float(self.tag_unigrams[tag])
+
+    def prob_e(self, word, tag):
+        return float(self.emit[(tag, word)]) / float(self.context[word])
+
+    def best_score(self, index, tag):
+        scores = []
+        if (index == 0):
+            return 0
+        word = self.current_sentence[index]
+        for previous_tag in self.possible_tags:
+                score = self.best_score(index - 1, previous_tag) + math.log(self.prob_t(tag,previous_tag)) + math.log(self.prob_e(word, previous_tag))
+                scores.append(score)
+        return 1
--- 2/test_model.py
+++ 2/test_model.py
@@ -3,32 +3,33 @@ import pytest
 from IPython import embed
 
 class TestModel:
-    @pytest.fixture
 
     @pytest.fixture
     def model(self, word_tag_sentences):
         return markov.Model(word_tag_sentences)
 
+    @pytest.fixture
     def example_model(self):
         word_tag_sentences = [
                 [("I", "NN"), ("am", "VB"), ("a", "DT"), ("sentence", "NN")],
-                [("I", "NN"), ("love", "VB"), ("the", "DT"), ("sun", "NN")]
+                [("I", "NN"), ("love", "VB"), ("the", "DT"), ("sun", "NN")],
+                [("I", "NN"), ("love", "VB"), ("your", "PP"), ("love", "NN")]
         ]
         return self.model(word_tag_sentences)
 
     def test_context(self):
         model = self.example_model()
-        assert model.context['I'] == 2
-        assert model.context['love'] == 1
+        assert model.context['I'] == 3
+        assert model.context['love'] == 3
         assert model.context['sun'] == 1
-        assert model.context['<s>'] == 2
+        assert model.context['<s>'] == 3
         assert model.context['</s>'] == 0 # okay, we transition the end of a sentence but don't count it, is that correct?
 
     def test_transition(self):
         model = self.example_model()
-        assert model.transition[('<s>', 'NN')] == 2
-        assert model.transition[('NN', '</s>')] == 2
-        assert model.transition[('NN', 'VB')] == 2
+        assert model.transition[('<s>', 'NN')] == 3
+        assert model.transition[('NN', '</s>')] == 3
+        assert model.transition[('NN', 'VB')] == 3
         assert model.transition[('VB', 'NN')] == 0
 
     def test_emit(self):
@@ -40,5 +41,20 @@ class TestModel:
 
     def test_possible_tags(self):
         model = self.example_model()
-        assert model.possible_tags == set(["NN", "VB", "DT"])
+        assert model.possible_tags == set(["NN", "VB", "DT", "PP"])
 
+    def test_prob_t(self):
+        model = self.example_model()
+        assert model.prob_t("DT", "VB") == 1
+        assert model.prob_t("VB", "NN") == 1
+
+    def test_prob_e(self):
+        model = self.example_model()
+        assert model.prob_e("love", "VB") == 2.0 / 3.0 # two times a verb, once a noun
+
+
+    #def test_best_score(self):
+        #model = self.example_model()
+        #model.test(["I", "am", "the", "sun"])
+        #assert model.best_score(1, "NN") == 1
+        #assert model.best_score(2, "VB") == 0.5
