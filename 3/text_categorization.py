a98f5c0d87b8c23c5ba26bd4eafe75c8225372e8
Robert Schaefer <robert.schaefer@student.hpi.de>
2015-06-29 19:01:55 +0200

    finished script part, super bad results

--- 3/text_categorization.py
+++ 3/text_categorization.py
@@ -1,19 +1,18 @@
 import os
 from IPython import embed
 
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 class DataSet:
-    def __init__(self, folder):
+    def __init__(self, folder, subset="training"):
         self.folder = folder
         self.data  = list()
         self.target = list()
-        training_folder = self.folder + "/training/"
-        self.target_names = sorted(next(os.walk(training_folder))[1])
+        subset_folder = self.folder + "/" + subset + "/"
+        self.target_names = sorted(next(os.walk(subset_folder))[1])
 
         for i,n in enumerate(self.target_names):
-                folder = training_folder + "/" + n + "/"
+                folder = subset_folder + "/" + n + "/"
                 for subdir, dirs, files in os.walk(folder):
                     for file in files:
                         with open(os.path.join(subdir,file),'r') as my_file:
@@ -23,8 +22,16 @@ class DataSet:
 
 
 if __name__ == "__main__":
-    count_vect = CountVectorizer()
-    input = DataSet("../ohsumed-first-20000-docs/")
-    X_train_counts = count_vect.fit_transform(input.data)
-    X_train_counts.shape
-    embed()
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.naive_bayes import MultinomialNB
+    from sklearn.feature_extraction.text import TfidfTransformer
+    from sklearn.pipeline import Pipeline
+    from sklearn import metrics
+
+    training = DataSet("../ohsumed-first-20000-docs/")
+    text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()) ])
+    text_clf = text_clf.fit(training.data, training.target)
+
+    test = DataSet("../ohsumed-first-20000-docs/", subset="test")
+    predicted = text_clf.predict(test.data)
+    print(metrics.classification_report(test.target, predicted, target_names=test.target_names))
